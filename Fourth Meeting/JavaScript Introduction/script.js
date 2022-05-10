4648214ff492009f623903b1aa64d77f9f5d0941
mAshrafDawood <ashraf.dawood2295@gmail.com>
2022-05-10 21:42:18 +0200

    Fourth meeting

---
+++ Fourth Meeting/JavaScript Introduction/script.js
@@ -0,0 +1,49 @@
+var str = "Hello";
+var num = 1;
+var float = 2.3;
+var flag = true;
+var arr = [1, true, "Hello World", 2];
+
+let x = 0;
+let y = 0;
+
+//const y = 2;
+//y = 3;
+
+//String, Number, Boolean, Array/List
+
+
+//What is the expected output ?
+//console.log(str);     //
+//console.log(num);     //
+//console.log(flag);    //
+//console.log(arr);     //
+
+//  JavaScript is weird
+//console.log(str + num); 
+//console.log(1 + "1");
+//arr = [1, true, "Hello World"];
+//console.log(arr + [1, 2]);
+//console.log(true + true);
+
+
+//console.log("Javascript is a super weird language");
+
+
+// + - * / % ** ++ --
+x = 5;
+y = 2;
+//onsole.log(x / y);
+//console.log(x ** y);
+
+x = 5;
+//Expected output
+//console.log(x);     //
+//console.log(++x);   //
+//console.log(x);     //
+// boolean resulting expression || && > >= == === < <=
+
+
+
+//  https://www.hackerrank.com/domains/tutorials/10-days-of-javascript
+//  https://jsisweird.com/
